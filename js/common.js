7b49c1613f2130517854e9dae7f585739ed6982b
propdeadman <83814503+propdeadman@users.noreply.github.com>
2022-07-08 02:33:31 +0400

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,35 @@
+// smooth_appearance
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        }
+    });
+}
+
+let options = {
+    threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
+// scroll_effect
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
